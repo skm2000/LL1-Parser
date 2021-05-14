e36b6e99be0f91ae4ff15b8a43aef2f0e4fd5df1
skm2000 <shubhamskm2000@gmail.com>
2021-05-14 14:26:02 +0530

    Added comments, improved printing format

--- Lexer/include/SymbolTable.h
+++ Lexer/include/SymbolTable.h
@@ -1,10 +1,33 @@
+/**
+ * * Assignment-3 of Compiler Design
+ * * @written by Shubham Kumar, Rajarshi Roychoudhary, Arka Maji, Susanka Mazumdar
+ * * Symbol Table header file
+*/
+
+
 #include"Token.h"
 
+/**
+ * ? SYMBOL TABLE CLASS
+ * * symtabfile = a file where my symbol table is kept (.tab);
+ * * tokenfile = tokens generated are kept (.tok) and this file is given as an input to the parser;
+ * * map<string,vector<Token>>string_tokens = A hashmap of tokens, with key as lexeme and Tokens as a vector;
+*/
+
 class SymbolTable{
+
     public:
+
         map<string,vector<Token>>string_tokens;
         fstream  symtabfile;
         fstream tokenfile;
+        
+        /**
+         * ? PARAMETERIZED CONSTRUCTOR SYMBOL TABLE
+         * * It takes two parameters, an symbol table file name and token file name;
+         * * Used to open both the files, error checks have been provided in case the file fails to open.
+         * * symtabfile << : This is used to write in a file
+        */
 
         SymbolTable(string symbol_table_file , string token_file ){ 
             symtabfile.open(symbol_table_file,ios::out);
@@ -17,12 +40,13 @@ class SymbolTable{
                 cout<<"Error: Token table file"<<"\n";
                 return;
             }
-            symtabfile << "============================SYMBOL TABLE==========================\n";
-            symtabfile <<"\n-------------------------------------------------------------------------\n";
-            symtabfile << "|TOKEN\t\t" << "TOKEN_ID\t\t" << "ROW\t\t" <<"COLUMN\t\t" << "SCOPE|\n";
-            symtabfile <<"\n-------------------------------------------------------------------------\n";
-            // symtabfile >> "Token" >>"\t\t" >> "Token_ID" >>"\t\t"
+            symtabfile << "======================================SYMBOL TABLE==========================================\n";
+            symtabfile << "\n--------------------------------------------------------------------------------------------\n";
+            symtabfile << "|LEXEME|\t\t" << "|TOKEN|\t\t" << "|ROW|\t\t" <<"|COLUMN|\t" << "|SCOPE|\n";
+            symtabfile << "--------------------------------------------------------------------------------------------\n";
         }
+
+
 
         bool insert_token(Token * t){
             if(t->token_id=="eof"){
@@ -33,7 +57,13 @@ class SymbolTable{
             }
             else{
                 tokenfile << t->token_id<<" ";
-                symtabfile << "|" << t->token << "\t\t" << t->token_id << "\t\t" << t->row << "\t\t" << t->column << "\t\t" << t->scope << "\n";
+                if(t->token == "string"){
+                    symtabfile <<"| "<<t->token<<"\t"<<t->token_id<<"\t\t\t"<<t->row<<"\t\t"<<t->column<<"\t\t"<<t->scope<<"|"<<endl;
+                }
+                else if(t->token_id == "stringToken" || t->token_id == "charToken" || t->token_id == "lessthan" || t->token_id == "greaterthan"){
+                    symtabfile <<"| "<<t->token<<"\t\t"<<t->token_id<<"\t\t"<<t->row<<"\t\t"<<t->column<<"\t\t"<<t->scope<<"|"<<endl;
+                }
+                else symtabfile <<"| "<<t->token<<"\t\t"<<t->token_id<<"\t\t\t"<<t->row<<"\t\t"<<t->column<<"\t\t"<<t->scope<<"|"<<endl;
             }
             if(string_tokens.find(t->token) == string_tokens.end()){
                 vector<Token> newVec;
@@ -48,7 +78,16 @@ class SymbolTable{
             }
         }
 
+        /**
+         * ? displaySym()
+         * * Used to display the symbol table. 
+        */
+
         void displaySym(){
+            cout << "================================SYMBOL TABLE=============================\n";
+            cout << "\n----------------------------------------------------------------------------\n";
+            cout << "|LEXEME|\t" << "|TOKEN|\t\t     " << "  |ROW|\t" <<"     |COLUMN|\t  " << "   |SCOPE|\n";
+            cout << "----------------------------------------------------------------------------\n";
             for(auto x : string_tokens){
                 for(auto y : x.second){
                     y.displayToken();
--- Lexer/include/Token.h
+++ Lexer/include/Token.h
@@ -1,5 +1,23 @@
+/**
+ * * Assignment-3 of Compiler Design
+ * * @written by Shubham Kumar, Rajarshi Roychoudhary, Arka Maji, Susanka Mazumdar
+ * * Token header file
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
+/**
+ * ? TOKEN CLASS
+ * * token_id = lexeme;
+ * * token = consists of assign_op, string,  char, parenthesis, relational_op and identifiers;
+ * * row = row number of that particular token;
+ * * column = column number of that particular token;
+ * * scope = scope number of that particular token, a counter kept which increases with  '{' and decreases with '}';
+ * * getter & setter = get and set the value of their respective fields
+ * * displayToken = prints the field of the token class
+*/
 
 class Token{
     public:
@@ -32,12 +50,21 @@ class Token{
         int getColumn(){
             return this->column;
         }
+        
         int getScope(){
             return this->scope;
         } 
 
         void displayToken(){
-            cout<<this->token<<"\t\t"<<this->token_id<<"\t\t"<<this->row<<"\t\t"<<this->column<<"\t\t"<<this->scope<<endl;
+            if(this->token == "string"){
+                cout<<"| "<<this->token<<"\t"<<this->token_id<<"\t\t\t"<<this->row<<"\t\t"<<this->column<<"\t\t"<<this->scope<<"|"<<endl;
+            }
+            else if(this->token_id == "stringToken" || this->token_id == "charToken" || this->token_id == "lessthan" || this->token_id == "greaterthan"){
+                cout<<"| "<<this->token<<"\t\t"<<this->token_id<<"\t\t"<<this->row<<"\t\t"<<this->column<<"\t\t"<<this->scope<<"|"<<endl;
+            }
+            else {
+                cout<<"| "<<this->token<<"\t\t"<<this->token_id<<"\t\t\t"<<this->row<<"\t\t"<<this->column<<"\t\t"<<this->scope<<"|"<<endl;
+            }    
         }
 };
 
