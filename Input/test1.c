f4026a2bfeb40dc0e6364e25e9480c4d6b58be9d
Rajarshi Roychoudhury <rroychoudhury2@gmail.com>
2021-05-19 16:06:08 +0530

    Scope resolved

---
+++ Input/test1.c
@@ -0,0 +1,41 @@
+void main(char c, string s){
+    {
+        char x = '1';
+        s = "abc";
+        {
+            {
+                while('a' < 'z'){
+
+                }
+            }
+        }
+        {
+            {
+                {
+                    while("abc" < "xyz"+"efgh"){
+                        string p = "efgg" + "123";
+                        string s = "alpha";
+                    }
+                }
+            }
+        }
+        string py = s;
+        string z;
+        while(s < py){
+            while(s<'a'){
+                ++x;
+                c = 'a'+c;
+                while(s>="aaczx"){
+                    c = c-'4';
+                }
+                while(c<'e'){
+                    s = s+"abcd";
+                }
+                string d = s+"Efgh"+"exyz";
+                z=d;
+            }
+            char y = 'y';
+            s = "hh";
+        }
+    }
+}
---
+++ Input/test2.c
@@ -0,0 +1,45 @@
+void main(char c, string s){
+    {
+        char x = '1';
+        s = "abc";
+        {
+            {
+                while('a' < 'z'){
+
+                }
+            }
+            s = "b" +s+"efgh"+"ae";
+            string abc,def,efgh;
+            string s;
+        }
+        {
+            {
+                {
+                    while("abc" < s){
+                        string p = "123";
+                        string s = "alpha";
+                    }
+                }
+            }
+            string s;
+        }
+        string py = s;
+        string z;
+        while(s < py){
+            while(s<'a'){
+                ++x;
+                c = 'a'+c;
+                while(s>="aaczx"){
+                    c = c-'4';
+                }
+                while(c<'e'){
+                    s = s+"abcd";
+                }
+                string d = s+"Efgh"+"exyz";
+                z=d;
+            }
+            char y = 'y';
+            s = "hh";
+        }
+    }
+}
